a26ba7f7a426676e2c534364e11f6d8c983c2645
jaimefernandez <jaimefernandez@localhost>
2021-03-23 12:46:06 +0100

    cambios

--- EjercicioProgramacion/src/ejercito/main.java
+++ EjercicioProgramacion/src/ejercito/main.java
@@ -13,7 +13,7 @@ public class main {
 		
 		Armada arm1=new Armada("Comandancia Naval de Sevilla","", 20838, 203, 17, 2, 1915);
 		Aire air1=new Aire("Cuartel General del Ejército del Aire", "Madrid", 20900, 412, 28,2, 1913);
-
+		Tierra tier1=new Tierra("Cuartel General del Ejercito de Tierra","Madrid",80000,132,1,1,1777);
 	}
 
 }
