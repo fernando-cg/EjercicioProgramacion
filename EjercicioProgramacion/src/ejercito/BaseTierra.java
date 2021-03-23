31f3362ee6befdc3ef8a817265b6f8b15ac16a50
adolfo <adolfo@localhost>
2021-03-23 10:45:38 +0100

    dllkd

--- EjercicioProgramacion/src/ejercito/BaseTierra.java
+++ EjercicioProgramacion/src/ejercito/BaseTierra.java
@@ -4,7 +4,7 @@ public class BaseTierra  extends Tierra{
 
 	public BaseTierra(String sede, int numTropas, int numNaves, int dia, int mes, int annio) {
 		super(sede, numTropas, numNaves, dia, mes, annio);
-		// TODO Auto-generated constructor stub
+		// TODO Auto-generated constructo
 	}
 
 }
