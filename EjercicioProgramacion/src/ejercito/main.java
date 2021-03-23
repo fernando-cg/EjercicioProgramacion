09f179c776ba16d792b86458c4c6e1ec3d2bb403
adolfo <adolfo@localhost>
2021-03-23 12:56:40 +0100

    sdsfdf

--- EjercicioProgramacion/src/ejercito/main.java
+++ EjercicioProgramacion/src/ejercito/main.java
@@ -10,6 +10,10 @@ public class main {
 		ArrayList<Armada>armadas=new ArrayList<Armada>();
 		ArrayList<Aire>aires=new ArrayList<Aire>();
 		ArrayList<Tierra>tierras=new ArrayList<Tierra>();
+		ArrayList<TropasArmada>tropasarmada=new ArrayList<TropasArmada>();
+		ArrayList<VehiculosArmada>vehiculosarmada=new ArrayList<VehiculosArmada>();
+		ArrayList<TropasAire>tropasaire=new ArrayList<TropasAire>();
+		ArrayList<VehiculosAire>vehiculosaire=new ArrayList<VehiculosAire>();
 		
 		Armada arm1=new Armada("Comandancia Naval de Sevilla","", 20838, 203, 17, 2, 1915);
 		Aire air1=new Aire("Cuartel General del Ejército del Aire", "Madrid", 20900, 412, 28,2, 1913);
