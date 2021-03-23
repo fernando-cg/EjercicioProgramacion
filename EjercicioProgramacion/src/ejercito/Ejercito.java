781b9826245a040edd775beca9094a7fb8c40a3c
jaimefernandez <jaimefernandez@localhost>
2021-03-23 09:47:57 +0100

    Actualizacion 9:47

--- EjercicioProgramacion/src/ejercito/Ejercito.java
+++ EjercicioProgramacion/src/ejercito/Ejercito.java
@@ -7,18 +7,49 @@ public abstract class Ejercito {
 private  Date fechaFundacion;
 private String sede;
 private int presupuesto;
-private int nºTropas;
-private int nºNaves;
+private int NumTropas;
+private int NumNaves;
 //Constructor
 public Ejercito(String sede, int nºTropas, int nºNaves,int dia,int mes,int annio) {
 	super();
 	this.sede = sede;
-	this.nºTropas = nºTropas;
-	this.nºNaves = nºNaves;
+	this.NumTropas = nºTropas;
+	this.NumNaves = nºNaves;
 	Calendar Fecha1=Calendar.getInstance();
-	Fecha1.set(annio, mes,dia);
+	Fecha1.set(annio, (mes-1),dia);
+	this.fechaFundacion=Fecha1.getTime();
 	
 }
 //Getters y Setters
+public Date getFechaFundacion() {
+	return fechaFundacion;
+}
+public void setFechaFundacion(Date fechaFundacion) {
+	this.fechaFundacion = fechaFundacion;
+}
+public String getSede() {
+	return sede;
+}
+public void setSede(String sede) {
+	this.sede = sede;
+}
+public int getPresupuesto() {
+	return presupuesto;
+}
+public void setPresupuesto(int presupuesto) {
+	this.presupuesto = presupuesto;
+}
+public int getNºTropas() {
+	return NumTropas;
+}
+public void setNºTropas(int nºTropas) {
+	this.NumTropas = nºTropas;
+}
+public int getNºNaves() {
+	return NumNaves;
+}
+public void setNºNaves(int nºNaves) {
+	this.NumNaves = nºNaves;
+}
 
 }
