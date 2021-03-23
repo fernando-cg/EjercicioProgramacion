5b64c8120f80e072dbf4c3e7d5286b4eb9ebf949
adolfo <adolfo@localhost>
2021-03-23 10:49:25 +0100

    alala

--- EjercicioProgramacion/src/ejercito/main.java
+++ EjercicioProgramacion/src/ejercito/main.java
@@ -1,9 +1,14 @@
 package ejercito;
 
+import java.util.ArrayList;
+
 public class main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		ArrayList<Armada>armadas=new ArrayList<Armada>();
+		ArrayList<Aire>aires=new ArrayList<Aire>();
 
 	}
 
