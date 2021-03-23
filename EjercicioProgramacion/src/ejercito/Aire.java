b002b68ea58e62ac05d9f5fe5af3ec1103a60893
jaimefernandez <jaimefernandez@localhost>
2021-03-23 12:18:23 +0100

    sfv

--- EjercicioProgramacion/src/ejercito/Aire.java
+++ EjercicioProgramacion/src/ejercito/Aire.java
@@ -3,10 +3,10 @@ package ejercito;
 public class Aire extends Ejercito{
 	
 	private int Id;
-	private static int IdSigiente;
+	private static int IdSigiente=0;
 	
 	//Constructor
-	public Aire(int id,String NombreBase, String sede, int numTropas, int numNaves, int dia, int mes, int annio) {
+	public Aire(String NombreBase, String sede, int numTropas, int numNaves, int dia, int mes, int annio) {
 		super(NombreBase, sede, numTropas, numNaves, dia, mes, annio);
 		Id=IdSigiente;
 		IdSigiente++;
--- EjercicioProgramacion/src/ejercito/Armada.java
+++ EjercicioProgramacion/src/ejercito/Armada.java
@@ -3,10 +3,10 @@ package ejercito;
 public class Armada extends Ejercito {
 
 	private int Id;
-	private static int IdSigiente;
+	private static int IdSigiente=0;
 	
 	//Constructor
-	public Armada(int id,String NombreBase, String sede, int numTropas, int numNaves, int dia, int mes, int annio) {
+	public Armada(String NombreBase, String sede, int numTropas, int numNaves, int dia, int mes, int annio) {
 		super(NombreBase, sede, numTropas, numNaves, dia, mes, annio);
 		Id=IdSigiente;
 		IdSigiente++;
