c41a1cc6ba2d71f3e3eedd6dd0f39bfd64bd5a13
jaimefernandez <jaimefernandez@localhost>
2021-03-23 10:12:00 +0100

    Actualizacion 10:11

--- EjercicioProgramacion/src/ejercito/Armada.java
+++ EjercicioProgramacion/src/ejercito/Armada.java
@@ -1,5 +1,10 @@
 package ejercito;
 
-public class Armada {
+public class Armada extends Ejercito {
+
+	public Armada(String sede, int numTropas, int numNaves, int dia, int mes, int annio) {
+		super(sede, numTropas, numNaves, dia, mes, annio);
+		// TODO Auto-generated constructor stub
+	}
 
 }
--- EjercicioProgramacion/src/ejercito/Tierra.java
+++ EjercicioProgramacion/src/ejercito/Tierra.java
@@ -1,5 +1,10 @@
 package ejercito;
 
-public class Tierra {
+public class Tierra extends Ejercito {
+
+	public Tierra(String sede, int numTropas, int numNaves, int dia, int mes, int annio) {
+		super(sede, numTropas, numNaves, dia, mes, annio);
+		// TODO Auto-generated constructor stub
+	}
 
 }
