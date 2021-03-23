2ef14b242cf94c862b100850085cfcadab726519
adolfo <adolfo@localhost>
2021-03-23 10:10:00 +0100

    aaaaaaa

--- EjercicioProgramacion/src/ejercito/Aire.java
+++ EjercicioProgramacion/src/ejercito/Aire.java
@@ -1,5 +1,12 @@
 package ejercito;
 
-public class Aire {
+public class Aire extends Ejercito{
+
+	public Aire(String sede, int numTropas, int numNaves, int dia, int mes, int annio) {
+		super(sede, numTropas, numNaves, dia, mes, annio);
+		// TODO Auto-generated constructor stub
+	}
+	
+	
 
 }
