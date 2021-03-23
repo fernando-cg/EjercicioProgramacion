9a647512ca12310452263c2ee05acedea1f4889b
adolfo <adolfo@localhost>
2021-03-23 12:24:49 +0100

    c c x c

--- EjercicioProgramacion/src/ejercito/main.java
+++ EjercicioProgramacion/src/ejercito/main.java
@@ -10,6 +10,9 @@ public class main {
 		ArrayList<Armada>armadas=new ArrayList<Armada>();
 		ArrayList<Aire>aires=new ArrayList<Aire>();
 		ArrayList<Tierra>tierras=new ArrayList<Tierra>();
+		
+		Armada arm1=new Armada("Comandancia Naval de Sevilla","", 20838, 203, 17, 2, 1915);
+		Aire air1=new Aire("Cuartel General del Ejército del Aire", "Madrid", 20900, 412, 28,2, 1913);
 
 	}
 
