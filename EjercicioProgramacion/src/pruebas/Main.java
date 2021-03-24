519a49ed03f9489618acf75808ec8c52922b1be0
Fernando <fetmanditocamachito@gmail.com>
2021-03-25 00:12:11 +0100

    conector

--- EjercicioProgramacion/src/pruebas/Main.java
+++ EjercicioProgramacion/src/pruebas/Main.java
@@ -1,15 +1,13 @@
 package pruebas;
 
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.InputStream;
+import javax.sound.sampled.LineUnavailableException;
+
 public class Main {
 
-	public static void main(String[] args) throws FileNotFoundException{
-		String sonido = "/Users/fernando_cg/Desktop/hbd.wav" ;
-		
-		InputStream in = new FileInputStream(sonido) ;
-		
+	public static void main(String[] args) throws LineUnavailableException{
+		 KeyMap k = new KeyMap((char) 1, "/home/hacker/Desktop/hbd") ;
+		 
+		 k.play() ;
 	}
 	
 
