ff4ffdf500c600ea9a8f03633fe455488809752f
adolfo <adolfo@localhost>
2021-03-24 09:50:04 +0100

    kj.jk

--- EjercicioProgramacion/src/ejercito/main.java
+++ EjercicioProgramacion/src/ejercito/main.java
@@ -19,7 +19,7 @@ public class main {
 		
 		Armada arm1=new Armada("Comandancia Naval de Sevilla","", 20838, 203, 17, 2, 1915);
 		Aire air1=new Aire("Cuartel General del Ejército del Aire", "Madrid", 20900, 412, 28,2, 1913);
-		Tierra tier1=new Tierra("Cuartel General del Ejército de Tierra","Madrid",80000,132,1,1,1767);
+		Tierra tier1=new Tierra("Cuartel General de l Ejército de Tierra","Madrid",80000,132,1,1,1767);
 		TropasArmada ta1=new TropasArmada(22000, null, null);
 		VehiculosArmada a1=new VehiculosArmada(0, null);
 		TropasAire tair1=new TropasAire(0, null, null);
