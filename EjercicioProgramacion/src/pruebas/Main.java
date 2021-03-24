5dd5c2caaaaf43e31ecf02b5fd48ba035cdafc12
fernando-cg <72653561+fernando-cg@users.noreply.github.com>
2021-03-24 20:25:15 +0100

    nn

---
+++ EjercicioProgramacion/src/pruebas/Main.java
@@ -0,0 +1,16 @@
+package pruebas;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+public class Main {
+
+	public static void main(String[] args) throws FileNotFoundException{
+		String sonido = "/Users/fernando_cg/Desktop/hbd.wav" ;
+		
+		InputStream in = new FileInputStream(sonido) ;
+		
+	}
+	
+
+}
