b9387c5f337e7907b7f7ef965c0e1e184532f756
fernando-cg <72653561+fernando-cg@users.noreply.github.com>
2021-03-25 08:21:14 +0100

    nn

--- EjercicioProgramacion/src/ejercito/main.java
+++ EjercicioProgramacion/src/ejercito/main.java
@@ -5,7 +5,6 @@ import java.util.ArrayList;
 public class main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		
 		ArrayList<Armada>armadas=new ArrayList<Armada>();
 		ArrayList<Aire>aires=new ArrayList<Aire>();
@@ -20,19 +19,12 @@ public class main {
 		Armada arm1=new Armada("Comandancia Naval de Sevilla","", 20838, 203, 17, 2, 1915);
 		Aire air1=new Aire("Cuartel General del Ejército del Aire", "Madrid", 20900, 412, 28,2, 1913);
 		Tierra tier1=new Tierra("Cuartel General de l  Ejército de Tierra","Madrid",80000,132,1,1,1767);
-		
-
-				TropasTierra tt1=new TropasTierra(10000, "Oficiales", "Cuartel General	del Ejercito de Tierra");
-
-				VehiculosTierra vt1=new VehiculosTierra(219, "Leopardo 2E");
-
-				TropasArmada ta1=new TropasArmada(8400, "Oficiales", "Comandancia Naval de Sevilla");
-
-				VehiculosArmada a1= new VehiculosArmada(5, "Fragatas");
-
-				TropasAire tair1= new TropasAire(9500, "Oficiales", "Base Aerea de Moron");
-
-				VehiculosAire va1=new VehiculosAire(73, "Eurofighter Typhoon");
+		TropasTierra tt1=new TropasTierra(10000, "Oficiales", "Cuartel General	del Ejercito de Tierra");
+		VehiculosTierra vt1=new VehiculosTierra(219, "Leopardo 2E");
+		TropasArmada ta1=new TropasArmada(8400, "Oficiales", "Comandancia Naval de Sevilla");
+		VehiculosArmada a1= new VehiculosArmada(5, "Fragatas");
+		TropasAire tair1= new TropasAire(9500, "Oficiales", "Base Aerea de Moron");
+		VehiculosAire va1=new VehiculosAire(73, "Eurofighter Typhoon");
 
 
 		
