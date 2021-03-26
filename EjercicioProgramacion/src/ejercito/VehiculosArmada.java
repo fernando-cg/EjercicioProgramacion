e9dab42ab4e06e75d01abdb1190c1ff60a80706f
adolfo <adolfo@localhost>
2021-03-26 12:31:44 +0100

    kkl

--- EjercicioProgramacion/src/ejercito/VehiculosArmada.java
+++ EjercicioProgramacion/src/ejercito/VehiculosArmada.java
@@ -1,16 +1,22 @@
 package ejercito;
 
+import escritura.Editor;
+
 public class VehiculosArmada extends Vehiculo {
 
 	public VehiculosArmada(int cantVehiculos, String tipo,String descripcion) {
 		super(cantVehiculos, tipo,descripcion);
 		// TODO Auto-generated constructor stub
+		
+		Editor e = new Editor("datos/VehiculosArmada.txt") ;
+		
+		e.escribir(cantVehiculos + "\t" + tipo + "\t" + descripcion +"\r\n");
 	}
 
 	@Override
 	public String toString() {
 		return "VehiculosAire Cantidad: " + getCantVehiculos() + "|Tipo" + getTipo()
-				+ "|Presupuesto=" + getPresupuesto()+"|Descripción del vehículo: "+getDescripcion();
+				+ "|Descripción del vehículo: "+getDescripcion();
 	}
 	
 	
