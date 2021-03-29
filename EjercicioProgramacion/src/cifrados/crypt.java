772aa3c6d951b8d698ab65cdb8836829d9f8ffc6
Fernando <fetmanditocamachito@gmail.com>
2021-03-29 14:24:04 +0200

    new

---
+++ EjercicioProgramacion/src/cifrados/crypt.java
@@ -0,0 +1,17 @@
+
+public class crypt {
+
+	public static void main(String[] args) 
+	{
+	    final String secretKey = "adob";
+	     
+	    String originalString = "fesac";
+	    String encryptedString = aes.encrypt(originalString, secretKey) ;
+	    String decryptedString = aes.decrypt(encryptedString, secretKey) ;
+	     
+	    System.out.println(originalString);
+	    System.out.println(encryptedString);
+	    System.out.println(decryptedString);
+	}
+
+}
