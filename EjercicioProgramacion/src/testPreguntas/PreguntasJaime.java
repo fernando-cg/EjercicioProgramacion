02ba97e5b5b5ad3f447470714a386c8bb057e577
adolfo <adolfo@air-de-adolfo.home>
2021-03-29 13:13:09 +0200

    asdfdf

--- EjercicioProgramacion/src/testPreguntas/PreguntasJaime.java
+++ EjercicioProgramacion/src/testPreguntas/PreguntasJaime.java
@@ -68,6 +68,59 @@ public class PreguntasJaime {
 			contador=contador+0;
 			JOptionPane.showMessageDialog(null,"Te lo cuento como error");
 		}
+		
+		
+		JOptionPane.showMessageDialog(null,"Responde Verdadero o Falso");
+		String p1=JOptionPane.showInputDialog("Cristobal Colón partió desde el Puerto de Palos, en Huelva");
+		if(p1.equalsIgnoreCase("Verdadero")) {
+			JOptionPane.showMessageDialog(null,"Respuesta Correcta");
+			contador++;
+		}
+		else if(p1.equalsIgnoreCase("Falso")){
+			JOptionPane.showMessageDialog(null,"Respuesta Incorrecta");
+		}
+		
+		
+		String p2=JOptionPane.showInputDialog(null,"Madrid fue la primera capital de España");
+		if(p2.equalsIgnoreCase("Verdadero")) {
+			JOptionPane.showMessageDialog(null,"Respuesta Incorrecta");
+		}
+		else if(p2.equalsIgnoreCase("Falso")){
+			JOptionPane.showMessageDialog(null,"Respuesta Correcta");
+			contador++;
+		}
+		
+		
+		String p3=JOptionPane.showInputDialog(null,"El 17 de Julio de 1936 comenzó la Guerra Civil Española");
+		if(p3.equalsIgnoreCase("Verdadero")) {
+			JOptionPane.showMessageDialog(null,"Respuesta Correcta");
+			contador++;
+		}
+		else if(p3.equalsIgnoreCase("Falso")) {
+			JOptionPane.showMessageDialog(null,"Respuesta Incorrecta");
+		}
+		
+		String p4=JOptionPane.showInputDialog(null,"Huelva es la ciudad más antigua de España");
+		
+		if(p4.equalsIgnoreCase("Verdadero")) {
+			JOptionPane.showMessageDialog(null,"Respuesta Incorrecta");
+		}
+		else if(p4.equalsIgnoreCase(p4)) {
+			JOptionPane.showMessageDialog(null,"Respuesta Correcta");
+			contador++;
+		}
+		
+
+		String p5=JOptionPane.showInputDialog(null,"La catedral de Sevilla es la catedral más grande del mundo");
+		if(p5.equalsIgnoreCase("Verdadero")) {
+			JOptionPane.showMessageDialog(null,"Respuesta Correcta");
+			contador++;
+		}
+		else if(p5.equalsIgnoreCase("Falso")) {
+			JOptionPane.showMessageDialog(null,"Respuesta Incorrecta");
+		}
+			
+		
 		if (contador >=3) {
 			JOptionPane.showMessageDialog(null,"Felicidades has aprobado con: "+contador+"Respuestas acertadas");
 
